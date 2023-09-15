8b6345b612e9baf97abf287ac2c3ae1c2f4265d3
surinder0322@gmail.com <sunny@Sunnys-Laptop.local>
2023-09-14 23:52:15 -0400

    added git files

--- src/sep14/TestGit.java
+++ src/sep14/TestGit.java
@@ -5,3 +5,26 @@ public class TestGit {
     System.out.print("Hello from git");
   }
 }
+/*
+
+1. git init ---> to initialize git repository
+
+2. touch index.html index.css index.js
+
+3. git add index.html
+or
+git add .
+
+4. git commit -m "message of your choice"
+
+git log ---> to check the commit history
+
+
+
+$ git commit -m index.css "added index file"
+error: pathspec 'added index file' did not match any file(s) known to git
+
+
+
+
+ */
