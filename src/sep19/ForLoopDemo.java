1a96828c5229ce0fb576509f1a305ee8b6190bfc
surinder0322@gmail.com <sunny@Sunnys-Laptop.local>
2023-09-21 19:20:05 -0400

    added for loop files

--- src/sep19/ForLoopDemo.java
+++ src/sep19/ForLoopDemo.java
@@ -8,11 +8,9 @@ public class ForLoopDemo {
         int sum = 0;
         for (int i = 1; i <= 10; i++) {
 
-            if(i % 2 == 0) // even
-            {
-
-                sum = sum + i;
-            }
+            if(i % 2 != 0) continue;
+            System.out.println(i);
+            sum  = sum +i;
         }
         System.out.println(sum);
     }
@@ -60,5 +58,17 @@ for(init ; condition  ; increment/decrement ){
 // statement
 
 }
+
+ */
+/*
+
+git add    --> staging area
+git commit    --> final save to local git
+git push    --> github
+
+git status   ---> to check current status of git
+
+git pull --- > to get the latest changes from github
+
 
  */
